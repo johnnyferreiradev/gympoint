5f7012d9e34044396eae18c720cac6b92b6a63bc
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-11-22 13:43:36 -0300

    Configuração do eslint, prettier e sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'johnny',
+  password: '12345678',
+  database: 'gympoint',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
