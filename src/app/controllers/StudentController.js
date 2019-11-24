4f4a489566bc88a7454618e0ff76ed83fbbfb902
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-11-24 12:55:37 -0300

    Criação do método update de estudantes

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -41,6 +41,47 @@ class StudentController {
       height,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number().integer(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validator fails' });
+    }
+
+    const studentId = req.params.id;
+    const studentEmail = req.body.email;
+
+    const student = await Student.findByPk(studentId);
+
+    // Verifica se o estudante existe
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+
+    // Verifica se o novo email já existe
+    if (studentEmail !== student.email) {
+      const studentExists = await Student.findOne({
+        where: { email: studentEmail },
+      });
+
+      if (studentExists != null) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    const { id, name, email, age, weight, height } = await student.update(
+      req.body
+    );
+
+    return res.json({ id, name, email, age, weight, height });
+  }
 }
 
 export default new StudentController();
